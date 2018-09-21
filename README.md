58cbcef21d72da18a6a1632f7beed9b917ab25ce
Oliver Sierra <oliver.sierra@ignition.sapient.com>
2018-09-20 21:06:52 -0700

    Basic API

---
+++ README.md
@@ -0,0 +1,4 @@
+# Text to Google Home
+
+## Google Home Notifier package
+https://www.npmjs.com/package/google-home-push
---
+++ index.js
@@ -0,0 +1,30 @@
+const GoogleHome = require('google-home-push');
+const express = require('express');
+
+const device = 'Living Room speaker';
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const myHome = new GoogleHome(device);
+
+app.get('/', (req, res) => {
+  res.send('GoogleHome Speak API');
+});
+
+app.get('/googlehome/api/speak/:text', (req, res) => {
+  let text = req.params.text;
+  if (!text) {
+    return res.status(400).send('No text was provided');
+  }
+
+  text = text.replace(/\+/g, ' ');
+
+  myHome.speak(text);
+
+  res.status(200).send(text);
+});
+
+app.listen(port, () => {
+  console.log(`Listening for request on port: ${ port }`);
+});
