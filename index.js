2f21b5714405d427ccc564342df0f05d612eb71d
Oliver Sierra <oliver.sierra@ignition.sapient.com>
2018-10-01 15:40:56 -0700

    Adding endpoint for custom speaker

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const GoogleHome = require('google-home-push');
 const express = require('express');
 
-const device = 'Living Room speaker';
 const port = process.env.PORT || 3000;
-
+const devices = {
+  'living': 'Home Speaker', // Set name or IP
+  'master': '192.168.86.1'
+}
 const app = express();
 
+function speakTo(text, device) {
+  const myHome = new GoogleHome(device);
+  text = text.replace(/\+/g, ' ');
+  myHome.speak(text);
+};
 
 app.get('/', (req, res) => {
   res.send('GoogleHome Speak API');
@@ -16,10 +23,18 @@ app.get('/googlehome/api/speak/:text', (req, res) => {
   if (!text) {
     return res.status(400).send('No text was provided');
   }
+  speakTo(text, devices.living);
 
-  const myHome = new GoogleHome(device);
-  text = text.replace(/\+/g, ' ');
-  myHome.speak(text);
+  res.status(200).send(text);
+});
+
+app.get('/googlehome/api/speakto/:device/:text', (req, res) => {
+  const device = req.params.device;
+  let text = req.params.text;
+  if (!text || !device || !devices[device]) {
+    return res.status(400).send('No text/Device was provided or device does is not registered');
+  }
+  speakTo(text, devices[device]);
 
   res.status(200).send(text);
 });
